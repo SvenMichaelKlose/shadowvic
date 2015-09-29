c4e225fe839b7c498ecff199c06450244dec9829
Sven Michael Klose <pixel@copei.de>
2015-09-29 16:37:57 +0200

    Don't include main.c in library.

--- make.sh
+++ make.sh
@@ -4,5 +4,5 @@ set -e
 
 echo "Compiling…"
 cd src; cc $@ -O3 -Wall -I../include -o ../shadowvic 6502.c 6561.c disassembler.c fb.c joystick.c main.c sync.c vic-20.c video.c; cd ..
-cd src; cc $@ -O3 -Wall -flto -c -I../include 6502.c 6561.c disassembler.c fb.c joystick.c main.c sync.c vic-20.c video.c; cd ..
+cd src; cc $@ -O3 -Wall -flto -c -I../include 6502.c 6561.c disassembler.c fb.c joystick.c sync.c vic-20.c video.c; cd ..
 ar cru -o libshadowvic.a src/*.o
