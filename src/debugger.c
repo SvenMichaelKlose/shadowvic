773835d06275f0afdaa95f565cec09a535e93239
Sven Michael Klose <pixel@copei.de>
2015-10-26 19:34:43 +0100

    Always show program counter value in prompt; not in register dump anymore.

--- src/debugger.c
+++ src/debugger.c
@@ -175,7 +175,7 @@ dump_flags ()
 int
 dump_registers (char * p)
 {
-    printf ("A: %02hx X: %02hx Y: %02hx S: %02hx PC: %04hx ", a, x, y, s, pc);
+    printf ("A: %02hx X: %02hx Y: %02hx S: %02hx ", a, x, y, s);
     dump_flags ();
     printf ("\n");
 
@@ -295,7 +295,7 @@ debugger_welcome_message ()
 
 void make_prompt (char * prompt)
 {
-    sprintf (prompt, "%04hx> ", next_disassembly_address);
+    sprintf (prompt, "%04hx> ", pc);
 }
 
 char *
