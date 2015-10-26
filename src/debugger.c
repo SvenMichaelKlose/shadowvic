6c3171677843afd6e0f6bf8f1bcb312c7d74a3ef
Sven Michael Klose <pixel@copei.de>
2015-10-26 09:54:43 +0100

    Added linenoise for the upcoming debugger. https://github.com/antirez/linenoise

--- src/debugger.c
+++ src/debugger.c
@@ -1,15 +1,61 @@
 /* shadowVIC – Copyright (c) 2015 Sven Michael Klose <pixel@hugbox.org> */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
+#include <string.h>
 
-void debugger ()
+#ifndef HISTORY
+#define HISTORY     "shadowvic-debugger.history"
+#endif
+
+void
+print_help ()
 {
-    printf ("Debugger is not yet implemented – exiting with code 255.\n");
-    exit (255);
+    printf ("Command overview:\n");
+    printf ("q       Quit shadowVIC.\n");
+    printf ("h       Print this help text.\n");
 }
 
-void init_debugger ()
+void
+debugger ()
+{
+    char * line;
+    char * p;
+
+    printf ("\nWelcome to the shadowVIC debugger.\n");
+    linenoiseHistoryLoad (HISTORY);
+
+    while ((line = linenoise ("> ")) != NULL) {
+        p = line;
+
+        /* Skip white space. */
+        while (*p && *p < ' ')
+            p++;
+
+        /* Ignore empty lines. */
+        if (!*p)
+            continue;
+
+        linenoiseHistoryAdd (p);
+        linenoiseHistorySave (HISTORY);
+
+        if (!strcmp (p, "q")) {
+            printf ("Bye! :)\n");
+            exit (255);
+        } else if (!strcmp (p, "h")) {
+            print_help ();
+        } else {
+            printf ("Unrecognized commend. ");
+            print_help ();
+        }
+        
+        free (line);
+    }
+}
+
+void
+init_debugger ()
 {
     signal (SIGINT, debugger);
 }
