496e6907b64f524d0cedd8fc4e8a1ccda99e0824
Sven Michael Klose <pixel@copei.de>
2015-10-30 11:29:36 +0100

    Introduce 'struct prg_info'.

--- src/prg-loader.c
+++ src/prg-loader.c
@@ -1,17 +1,36 @@
 /* shadowVIC – Copyright (c) 2015 Sven Michael Klose <pixel@hugbox.org> */
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
 #include "types.h"
 #include "6502.h"
+#include "prg-loader.h"
 
 void
-load_prg (char * name)
+load_prg (struct prg_info * i)
 {
-    address load_addr;
+    int    fd;
 
-    FILE * f = fopen (name, "r");
-    (void) fread (&load_addr, 2, 1, f);
-    (void) fread (&m[load_addr], 65536, 1, f);
-    fclose (f);
+    if ((fd = open (i->pathname, O_RDONLY)) < 0) {
+        printf ("Could not open '%s': %s\n", i->pathname, strerror (errno));
+        return;
+    }
+    if (i->is_relocated) {
+        (void) read (fd, &i->load_addr, 2);
+        i->load_addr = m[0x2b] + (m[0x2c] << 8);
+        printf ("Relocating '%s' to %04hx.\n", i->pathname, i->load_addr);
+    } else {
+        (void) read (fd, &i->load_addr, 2);
+        printf ("Loading '%s' to %04hx.\n", i->pathname, i->load_addr);
+    }
+
+    i->size = read (fd, &m[i->load_addr], 65536);
+    printf ("Loaded %d bytes.\n", i->size);
+    close (fd);
 }
--- src/prg-loader.h
+++ src/prg-loader.h
@@ -1,3 +1,10 @@
 /* shadowVIC – Copyright (c) 2015 Sven Michael Klose <pixel@hugbox.org> */
 
-void load_prg (char * name);
+struct prg_info {
+    char *  pathname;
+    address load_addr;
+    size_t  size;
+    int     is_relocated;
+};
+
+void load_prg (struct prg_info *);
